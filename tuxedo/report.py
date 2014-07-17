0aff6f7713e1703d5d37525a8b6d677f4568ee84
Liang Bo Wang <ccwang002@gmail.com>
2014-07-17 20:35:46 +0800

    rewrite report to be inheritable

---
+++ tuxedo/report.py
@@ -0,0 +1,17 @@
+class Report:
+    pass
+
+class PageTemplate:
+    def __init__(name):
+        pass
+
+_CAVEAT_MSG = '''\
+New output result is under {!s}.
+
+The folder can be downloaded and viewed locally.
+Quick remainder for serving current folder through http:
+
+    $ python3 -m http.server
+    # Serving HTTP on 0.0.0.0 port 8000 ...
+'''
+
