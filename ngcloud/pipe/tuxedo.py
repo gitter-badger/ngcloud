69ccb15d93ee0c91df392c64429b7064817e3634
Liang Bo Wang (MSRA) <ccwang002@gmail.com>
2014-07-23 20:41:09 +0800

    add Tuxedo implementation based on abstract class
    QC static file copying has not been implemented

--- ngcloud/pipe/tuxedo.py
+++ ngcloud/pipe/tuxedo.py
@@ -1,2 +1,31 @@
-class TuxedoReader:
-    pass
+from pathlib import Path
+import ngcloud as ng
+from ngcloud.report import Stage, Report
+
+logger = ng._create_logger(__name__)
+_here = Path(__file__).parent
+
+
+class IndexStage(Stage):
+    template_name = 'index'
+    template_root = _here / 'report' / 'templates'
+
+class QCStage(Stage):
+    template_name = 'qc'
+    template_root = _here / 'report' / 'templates'
+
+class TophatStage(Stage):
+    template_name = 'tophat'
+    template_root = _here / 'report' / 'templates'
+
+class TuxedoReport(Report):
+    """NGCloud report class of Tuxedo pipeline."""
+
+    def template_config(self):
+        self.stage_template_cls = [
+            IndexStage,
+            QCStage,
+            TophatStage,
+            # CufflinkStage
+        ]
+        self.static_root = _here / 'report' / 'static'
