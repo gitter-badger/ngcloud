dfb6df12a5b390fcf5af3b120a57f8d448961ddd
Liang Bo Wang (MSRA) <ccwang002@gmail.com>
2014-07-24 19:38:23 +0800

    update logging format

--- ngcloud/__init__.py
+++ ngcloud/__init__.py
@@ -3,7 +3,9 @@ import logging
 __version__ = '0.0.4'
 
 _log_formatter = logging.Formatter(
-    '[%(asctime)s][%(levelname)s][%(name)-20s][%(funcName)s] %(message)s')
+    '[%(asctime)s][%(levelname)-7s][%(name)-8s][%(funcName)-8s] %(message)s',
+    '%Y-%m-%d %H:%M:%S'
+)
 
 def _create_logger(name):
     logger = logging.getLogger(name)
