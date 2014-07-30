0f91d0b6903297fc0c8b146d41a4d659da930783
Liang Bo Wang <ccwang002@gmail.com>
2014-07-30 21:47:51 +0800

    add mypipe module for my_stage demo

---
+++ examples/my_first_pipe/mypipe.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from ngcloud.report import Stage, Report
+
+here = Path(__file__).parent
+template_root = here / "report" / "templates"
+
+class IndexStage(Stage):
+    template_find_paths = template_root
+    template_entrances = "index.html"
+
+class MyStage(Stage):
+    template_find_paths = template_root
+    template_entrances = "mystage.html"
+
+    def parse(self):
+        self.result_info['mapped_rate'] = "50%"
+
+        # real way
+        mapped_rate = "(Unknown)"
+        summary_txt = self.job_info.root_path / "my_stage" / "summary.txt"
+        with summary_txt.open() as summary:
+            for line in summary:
+                if line.startswith("Overall"):
+                    mapped_rate = line.strip().split(': ')[-1]
+
+        self.result_info['mapped_rate'] = mapped_rate
+
+class MyReport(Report):
+    def template_config(self):
+        self.stage_template_cls = [IndexStage, MyStage]
+        self.static_roots = here / "report" / "static"
