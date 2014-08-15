ba6a959abaa84d0ba5fd78b1f04bd102f8413208
Liang Bo Wang (MSRA) <ccwang002@gmail.com>
2014-08-15 17:08:32 +0800

    use new API for static files copying in QC stage

--- ngcloud/pipe/tuxedo.py
+++ ngcloud/pipe/tuxedo.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import ngcloud as ng
 from ngcloud.report import Stage, Report
-from ngcloud.util import copy, discover_file_by_patterns
 from ngcloud.pipe import (
     _get_builtin_template_root,
     get_shared_template_root, get_shared_static_root
@@ -13,6 +12,7 @@ _find_paths = [
     get_shared_template_root(),
     _get_builtin_template_root() / 'tuxedo',
 ]
+
 
 class TuxedoBaseStage(Stage):
     template_find_paths = _find_paths
@@ -31,49 +31,17 @@ class IndexStage(TuxedoBaseStage):
 
 class QCStage(TuxedoBaseStage):
     template_entrances = 'qc.html'
-
-    def copy_static(self):
-        """Copy needed file for report in QC stage.
-
-        - qc summary img *(not implemented)*
-        - qc img by sample
-
-        """
-        self.copy_static_summary()
-        self.copy_static_sample()
-
-    def copy_static_summary(self):
-        """Copy QC summary pics to static/qc_overall"""
-
-        file_patterns = [
-            'per_base_quality.png',
-            'per_base_sequence_content.png',
-        ]
-        dest_root = self.report_root / 'static' / 'qc_overall'
-        src_root = self.job_info.root_path / '1_fastqc' / 'overall'
-
-        if not dest_root.exists():
-            dest_root.mkdir(parents=True)
-
-        for fp in file_patterns:
-            copy(src_root / fp, dest_root)
-
-    def copy_static_sample(self):
-        """Copy QC sample pics to static/qc_sample/<sample_name>"""
-        file_patterns = ['*.png']
-
-        all_src_root = self.job_info.root_path / '1_fastqc' / 'output'
-        all_dest_root = self.report_root / 'static' / 'qc_sample'
-
-        for sample in self.job_info.sample_list:
-            sp_src_root = (
-                all_src_root / "{}_fastqc".format(sample.name) / "Images"
-            )
-            sp_dest_root = all_dest_root / sample.full_name / "pics"
-            sp_dest_root.mkdir(parents=True)
-            file_list = discover_file_by_patterns(sp_src_root, file_patterns)
-            for f in file_list:
-                copy(f, sp_dest_root)
+    result_foldername = 'fastqc'
+    embed_result_joint = [
+        {'src': 'overall',
+         'patterns': ['per_base_quality.png', 'per_base_sequence_content.png'],
+         'dest': 'qc_overall'},
+    ]
+    embed_result_persample = [
+        {'src': 'output',
+         'patterns': ['Images/*.png'],
+         'dest': 'qc_sample/pics'},
+    ]
 
 
 class TophatStage(TuxedoBaseStage):
