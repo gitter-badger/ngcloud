23f7176da2117801f10a7c6802ae2a172671c242
Liang Bo Wang <ccwang002@gmail.com>
2014-08-17 19:52:41 +0800

    update example to embed tuxedo pipeline

--- examples/gen_tuxedo_ngreport.py
+++ examples/gen_tuxedo_ngreport.py
@@ -1,34 +1,22 @@
 from pathlib import Path
-import shutil
+import logging
 from ngcloud.report import gen_report
-from ngcloud.util import strify_path
-from ngcloud import _create_logger
-
-logger = _create_logger(__name__)
-
-def cleanup_output(output_root):
-    if not output_root.exists():
-        output_root.mkdir()
-        return
-
-    logger.info('Remove previous outputs under output')
-    previous_output = [
-        strify_path(p) for p in output_root.iterdir()
-        if p.is_dir() and p.name.startswith('report_')
-    ]
-    logger.warn(
-        'These ouput reports will be removed: %s' % ', '.join(previous_output)
-    )
-    for pre in previous_output:
-        shutil.rmtree(pre)
 
 if __name__ == '__main__':
     output_root = Path('output')
-    cleanup_output(output_root)
+    if not output_root.exists():
+        output_root.mkdir()
+
+    console = logging.StreamHandler()
+    console.setFormatter(logging.Formatter(
+        "[%(levelname)s][%(name)s] %(message)s"))
+    ng_loggers = logging.getLogger("ngcloud")
+    ng_loggers.addHandler(console)
+    ng_loggers.setLevel(logging.INFO)
 
     gen_report(
         pipe_report_cls='ngcloud.pipe.tuxedo.TuxedoReport',
-        job_dir=Path("job_9527_tuxedo_minimal"),
+        job_dir=Path("job_tuxedo_minimal"),
         out_dir=output_root
     )
 
