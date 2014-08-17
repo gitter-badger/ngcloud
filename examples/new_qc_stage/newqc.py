bb711f93ff048df85dc5185bf88c2c0555aeba27
Liang Bo Wang <ccwang002@gmail.com>
2014-08-17 20:54:13 +0800

    update new QC example

--- examples/new_qc_stage/newqc.py
+++ examples/new_qc_stage/newqc.py
@@ -1,16 +1,18 @@
 import logging
 from pathlib import Path
 from ngcloud.report import Report, main as _main
-from ngcloud.pipe import get_shared_static_root, get_shared_template_root
+from ngcloud.pipe import (
+    get_shared_static_root
+)
 from ngcloud.pipe.tuxedo import QCStage
 
 logger = logging.getLogger("external.%s" % __name__)
 
 class NewQCStage(QCStage):
-    template_find_paths = [
-        Path('report', 'templates'),
-        get_shared_template_root(),
-    ]
+    template_find_paths = QCStage.template_find_paths[:]
+    template_find_paths.insert(
+        0, Path('report', 'templates')
+    )
     template_entrances = 'newqc.html'
     embed_result_joint = []
     embed_result_persample = [
@@ -34,6 +36,7 @@ class QCOnly(Report):
     stage_classnames = [NewQCStage]
     static_roots = [
         get_shared_static_root(),
+        '../../template_dev/public',
     ]
 
 def main():
