7bad0a4e02414d2e0f459e8097a220570e3ead76
Liang Bo Wang <ccwang002@gmail.com>
2014-08-18 20:22:09 +0800

    display real fastqc status

--- examples/new_qc_stage/newqc.py
+++ examples/new_qc_stage/newqc.py
@@ -1,10 +1,12 @@
 import logging
 from pathlib import Path
+from collections import OrderedDict
 from ngcloud.report import Report, main as _main
 from ngcloud.pipe import (
     get_shared_static_root
 )
 from ngcloud.pipe.tuxedo import QCStage
+from ngcloud.util import open
 
 logger = logging.getLogger("external.%s" % __name__)
 
@@ -21,12 +23,55 @@ class NewQCStage(QCStage):
          'dest': 'qc_sample/pics'},
     ]
 
+    FASTQC_FILENAME = {
+        'Per base sequence quality': 'per_base_quality.png',
+        'Per sequence quality scores': 'per_sequence_quality.png',
+        'Per base sequence content': 'per_base_sequence_content.png',
+        'Per base GC content': 'per_base_gc_content.png',
+        'Per sequence GC content': 'per_sequence_gc_content.png',
+        'Per base N content': 'per_base_n_content.png',
+        'Sequence Length Distribution': 'sequence_length_distribution.png',
+        'Sequence Duplication Levels': 'duplication_levels.png',
+    }
+    FASTQC_NOFILE = [
+        'Basic Statistics',
+        'Overrepresented sequences',
+        'Kmer Content'
+    ]
+    FASTQC_GLYPH = {
+        'pass': 'glyphicon-ok',
+        'warn': 'glyphicon-exclamation-sign',
+        'fail': 'glyphicon-remove'
+    }
+
+    def read_fastqc_data(self, sample):
+        qc_info = OrderedDict()
+        qc_data_pth = self.result_root / sample.full_name / 'fastqc_data.txt'
+        with open(qc_data_pth) as qc_data:
+            for line in qc_data:
+                new_sec = line.startswith('>>')
+                sec_end = line.startswith('>>END_MODULE')
+                if new_sec and not sec_end:
+                    qc_desc, result = line.rstrip()[2:].rsplit('\t', 1)
+                    qc_info[qc_desc] = result
+        logger.debug(
+            "Sample {}'s qc_info: {}".format(sample.full_name, qc_info)
+        )
+        return qc_info
+
     def parse(self):
+        self.result_info['qc_info'] = dict()
         for sample in self.job_info.sample_list:
-            pass
+            qc_info = self.read_fastqc_data(sample)
+            self.result_info['qc_info'][sample.full_name] = qc_info
         self.result_info['stage_mapping'] = [
             ('qc', 'newqc.html', 'Quality Control')
         ]
+        self.result_info.update({
+            "FASTQC_FILENAME": self.FASTQC_FILENAME,
+            "FASTQC_NOFILE": self.FASTQC_NOFILE,
+            "FASTQC_GLYPH": self.FASTQC_GLYPH
+        })
 
 
 class QCOnly(Report):
