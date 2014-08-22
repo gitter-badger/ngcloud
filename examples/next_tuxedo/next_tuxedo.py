3a2f928aaebde4fe3a0bc7c08918fe66731e3449
Liang Bo Wang (MSRA) <ccwang002@gmail.com>
2014-08-22 18:50:38 +0800

    add parser for align_summary.txt in TophatStage

--- examples/next_tuxedo/next_tuxedo.py
+++ examples/next_tuxedo/next_tuxedo.py
@@ -1,3 +1,4 @@
+import re
 import logging
 from pathlib import Path
 from ngcloud.report import main as _main
@@ -6,9 +7,61 @@ from ngcloud.util import open
 
 logger = logging.getLogger("external.%s" % __name__)
 
+# According to the threads on Tuxedo Tools User Group,
+# We read the alignment status from aling_summary.txt
+# https://groups.google.com/d/msg/tuxedo-tools-users/s_oQRTwCuXA/_0PL4thE8OYJ
+
+_align_txt_left = (
+    r"Left reads:\n"
+    r"\s*Input\s*:\s*(?P<left_input>\d+)\n"
+    r"\s*Mapped\s*:\s*(?P<left_map>\d+) \([ 0-9.%]* of input\)\n"
+    r"\s*of these:\s*(?P<left_multimap>\d+) \([ 0-9.%]*\) "
+    r"have multiple alignments \((?P<left_multicount>\d+) have >1\)\n"
+)
+_align_txt_right = _align_txt_left.replace(
+    'Left', 'Right').replace('left', 'right')
+_align_txt_aligned = (
+    r"\s*Aligned pairs:\s*(?P<align_pair>\d+)\n"
+    r"\s*of these:\s*(?P<align_multi>\d+) \([ 0-9.%]*\) .*\n"
+    r"\s*(?P<align_discord>\d+) \([ 0-9.%]*\) are discordant alignments\n"
+)
+_extract_separate = re.compile(
+    _align_txt_left + _align_txt_right, re.MULTILINE
+).search
+_extract_align = re.compile(
+    _align_txt_aligned, re.MULTILINE
+).search
+
 class TophatStage(tuxedo.TophatStage):
+    result_foldername = 'tophat'
     template_find_paths = tuxedo.TuxedoBaseStage.template_find_paths[:]
-    template_find_paths.insert(0, Path('template'))
+    template_find_paths.insert(0, Path('templates'))
+
+    def parse(self):
+        super().parse()
+        for group, sample_list in self.job_info.sample_group.items():
+            info = self.parse_sample(group, sample_list)
+            logger.debug("Get info: {!r}".format(info))
+
+    def parse_sample(self, group, sample_list):
+        logger.debug("Reading align_summary.txt")
+        align_txt = self.result_root / group / 'align_summary.txt'
+        with open(align_txt) as align_summary:
+            raw_string = align_summary.read()
+
+        match_sep = _extract_separate(raw_string)
+        if not match_sep:
+            raise ValueError("Cannot get left/right info in align_summary.txt")
+        match_align = _extract_align(raw_string)
+        if not match_align:
+            raise ValueError("Cannot get pair info in align_summary.txt")
+
+        info_dict = {
+            k: int(v)
+            for m in [match_sep, match_align]
+            for k, v in m.groupdict().items()
+        }
+        return info_dict
 
 class TuxedoReport(tuxedo.TuxedoReport):
     stage_classnames = [
