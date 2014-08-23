fa988527ffd8184ff51f89539d7bd495ea042e76
Liang Bo Wang (MSRA) <ccwang002@gmail.com>
2014-08-23 16:07:08 +0800

    update builtin Tuxedo template

--- ngcloud/pipe/tuxedo.py
+++ ngcloud/pipe/tuxedo.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 from collections import OrderedDict, namedtuple
 import ngcloud as ng
@@ -57,6 +58,30 @@ Miscellaneous:
 - :class:`OverSeq`
 
 """
+
+# According to the threads on Tuxedo Tools User Group,
+# We read the alignment status from aling_summary.txt
+# https://groups.google.com/d/msg/tuxedo-tools-users/s_oQRTwCuXA/_0PL4thE8OYJ
+_align_txt_left = (
+    r"Left reads:\n"
+    r"\s*Input\s*:\s*(?P<left_input>\d+)\n"
+    r"\s*Mapped\s*:\s*(?P<left_map>\d+) \([ 0-9.%]* of input\)\n"
+    r"\s*of these:\s*(?P<left_multimap>\d+) \([ 0-9.%]*\) "
+    r"have multiple alignments \((?P<left_multicount>\d+) have >1\)\n"
+)
+_align_txt_right = _align_txt_left.replace(
+    'Left', 'Right').replace('left', 'right')
+_align_txt_aligned = (
+    r"\s*Aligned pairs:\s*(?P<align_pair>\d+)\n"
+    r"\s*of these:\s*(?P<align_multi>\d+) \([ 0-9.%]*\) .*\n"
+    r"\s*(?P<align_discord>\d+) \([ 0-9.%]*\) are discordant alignments\n"
+)
+_extract_separate = re.compile(
+    _align_txt_left + _align_txt_right, re.MULTILINE
+).search
+_extract_align = re.compile(
+    _align_txt_aligned, re.MULTILINE
+).search
 
 OverSeq = namedtuple(
     "OverSeq", ["seq", "count", "percentage", "possible_source"])
@@ -156,8 +181,52 @@ class QCStage(TuxedoBaseStage):
 
 class TophatStage(TuxedoBaseStage):
     """Tophat stage for Tuxedo pipeline"""
+    result_foldername = 'tophat'
     template_entrances = 'tophat.html'
 
+    DETAIL_SEP = [
+        ('Input', 'input'),
+        ('Mapped', 'map'),
+        ('Multi', 'multimap'),
+        ('Multi Count', 'multicount')
+    ]
+    DETAIL_PAIR = [
+        ('Aligned pairs', 'pair'),
+        ('Mutli-align pairs', 'multi'),
+        ('Discordants pairs', 'discord')
+    ]
+
+    def parse(self):
+        super().parse()
+        self.set_const()
+        self.result_info['detail_info'] = dict()
+        for group, sample_list in self.job_info.sample_group.items():
+            detail_info = self.parse_sample(group, sample_list)
+            self.result_info['detail_info'][group] = detail_info
+
+    def set_const(self):
+        for const in ['DETAIL_SEP', 'DETAIL_PAIR']:
+            self.result_info[const] = getattr(TophatStage, const)
+
+    def parse_sample(self, group, sample_list):
+        logger.debug("Reading align_summary.txt")
+        align_txt = self.result_root / group / 'align_summary.txt'
+        with open(align_txt) as align_summary:
+            raw_string = align_summary.read()
+
+        match_sep = _extract_separate(raw_string)
+        if not match_sep:
+            raise ValueError("Cannot get left/right info in align_summary.txt")
+        match_align = _extract_align(raw_string)
+        if not match_align:
+            raise ValueError("Cannot get pair info in align_summary.txt")
+
+        info_dict = {
+            k: int(v)
+            for m in [match_sep, match_align]
+            for k, v in m.groupdict().items()
+        }
+        return info_dict
 
 class TuxedoReport(Report):
     """NGCloud report class of Tuxedo pipeline."""
