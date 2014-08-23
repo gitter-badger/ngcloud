83d48438c5a4eb1c58d037d3f407d57e43a76dd1
Liang Bo Wang (MSRA) <ccwang002@gmail.com>
2014-08-23 16:07:34 +0800

    use updated builtin Tuxedo

--- examples/next_tuxedo/next_tuxedo.py
+++ examples/next_tuxedo/next_tuxedo.py
@@ -3,14 +3,13 @@ import logging
 from pathlib import Path
 from ngcloud.report import main as _main
 from ngcloud.pipe import tuxedo
-from ngcloud.util import open
+from ngcloud.util import open  # flake8: noqa
 
 logger = logging.getLogger("external.%s" % __name__)
 
 # According to the threads on Tuxedo Tools User Group,
 # We read the alignment status from aling_summary.txt
 # https://groups.google.com/d/msg/tuxedo-tools-users/s_oQRTwCuXA/_0PL4thE8OYJ
-
 _align_txt_left = (
     r"Left reads:\n"
     r"\s*Input\s*:\s*(?P<left_input>\d+)\n"
@@ -33,53 +32,8 @@ _extract_align = re.compile(
 ).search
 
 class TophatStage(tuxedo.TophatStage):
-    result_foldername = 'tophat'
     template_find_paths = tuxedo.TuxedoBaseStage.template_find_paths[:]
     template_find_paths.insert(0, Path('templates'))
-
-    DETAIL_SEP = [
-        ('Input', 'input'),
-        ('Mapped', 'map'),
-        ('Multi', 'multimap'),
-        ('Multi Count', 'multicount')
-    ]
-    DETAIL_PAIR = [
-        ('Aligned pairs', 'pair'),
-        ('Mutli-align pairs', 'multi'),
-        ('Discordants pairs', 'discord')
-    ]
-
-    def parse(self):
-        super().parse()
-        self.set_const()
-        self.result_info['detail_info'] = dict()
-        for group, sample_list in self.job_info.sample_group.items():
-            detail_info = self.parse_sample(group, sample_list)
-            self.result_info['detail_info'][group] = detail_info
-
-    def set_const(self):
-        for const in ['DETAIL_SEP', 'DETAIL_PAIR']:
-            self.result_info[const] = getattr(TophatStage, const)
-
-    def parse_sample(self, group, sample_list):
-        logger.debug("Reading align_summary.txt")
-        align_txt = self.result_root / group / 'align_summary.txt'
-        with open(align_txt) as align_summary:
-            raw_string = align_summary.read()
-
-        match_sep = _extract_separate(raw_string)
-        if not match_sep:
-            raise ValueError("Cannot get left/right info in align_summary.txt")
-        match_align = _extract_align(raw_string)
-        if not match_align:
-            raise ValueError("Cannot get pair info in align_summary.txt")
-
-        info_dict = {
-            k: int(v)
-            for m in [match_sep, match_align]
-            for k, v in m.groupdict().items()
-        }
-        return info_dict
 
 class TuxedoReport(tuxedo.TuxedoReport):
     stage_classnames = [
