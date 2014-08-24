3e7c44dc00f8bc3d63c17c39759e7f20e56fffa2
Liang Bo Wang <ccwang002@gmail.com>
2014-08-24 21:03:30 +0800

    add overall alignment rate

--- examples/next_tuxedo/next_tuxedo.py
+++ examples/next_tuxedo/next_tuxedo.py
@@ -1,5 +1,7 @@
 import re
 import logging
+# TODO: 3.3 compatiblity
+import statistics as stats
 from pathlib import Path
 from ngcloud.report import SummaryStage, main as _main
 from ngcloud.pipe import tuxedo
@@ -16,7 +18,38 @@ class TuxedoBaseStage(tuxedo.TuxedoBaseStage):
         return self.job_info.sample_group
 
 class TophatStage(TuxedoBaseStage, tuxedo.TophatStage):
-    pass
+    def parse(self):
+        super().parse()
+        logger.debug('Get overall pair align rate')
+        self.compute_overall()
+
+    def compute_overall(self):
+        detail = self.result_info['detail_info']
+        sep_rate = dict()
+        pair_rate = dict()
+        for group in self.sample_group:
+            left_all = detail[group]['left_input']
+            right_all = detail[group]['right_input']
+            if not left_all == right_all:
+                raise ValueError(
+                    "Unequal numbers of read for pair-end sample"
+                    "{}".format(group))
+            sep_rate[group] = [
+                detail[group][d + '_map'] / left_all
+                for d in ['left', 'right']
+            ]
+            pair_rate[group] = detail[group]['align_pair'] / left_all
+
+        self.result_info['sep_rate'] = sep_rate
+        self.result_info['pair_rate'] = pair_rate
+        self.result_info['overall_sep_rate'] = stats.mean(
+            rate
+            for group_rates in sep_rate.values()
+            for rate in group_rates
+        )
+        self.result_info['overall_pair_rate'] = stats.mean(
+            rate for rate in pair_rate.values()
+        )
 
 class IndexStage(SummaryStage, TuxedoBaseStage, tuxedo.IndexStage):
     pass
