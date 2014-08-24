19b50e7d9d2b05132fb5b3f475f3ab5c3b28d3a4
Liang Bo Wang <ccwang002@gmail.com>
2014-08-24 16:39:50 +0800

    clean old tophat stuff

--- examples/next_tuxedo/next_tuxedo.py
+++ examples/next_tuxedo/next_tuxedo.py
@@ -7,30 +7,6 @@ from ngcloud.util import open  # flake8: noqa
 
 logger = logging.getLogger("external.%s" % __name__)
 
-# According to the threads on Tuxedo Tools User Group,
-# We read the alignment status from aling_summary.txt
-# https://groups.google.com/d/msg/tuxedo-tools-users/s_oQRTwCuXA/_0PL4thE8OYJ
-_align_txt_left = (
-    r"Left reads:\n"
-    r"\s*Input\s*:\s*(?P<left_input>\d+)\n"
-    r"\s*Mapped\s*:\s*(?P<left_map>\d+) \([ 0-9.%]* of input\)\n"
-    r"\s*of these:\s*(?P<left_multimap>\d+) \([ 0-9.%]*\) "
-    r"have multiple alignments \((?P<left_multicount>\d+) have >1\)\n"
-)
-_align_txt_right = _align_txt_left.replace(
-    'Left', 'Right').replace('left', 'right')
-_align_txt_aligned = (
-    r"\s*Aligned pairs:\s*(?P<align_pair>\d+)\n"
-    r"\s*of these:\s*(?P<align_multi>\d+) \([ 0-9.%]*\) .*\n"
-    r"\s*(?P<align_discord>\d+) \([ 0-9.%]*\) are discordant alignments\n"
-)
-_extract_separate = re.compile(
-    _align_txt_left + _align_txt_right, re.MULTILINE
-).search
-_extract_align = re.compile(
-    _align_txt_aligned, re.MULTILINE
-).search
-
 class TophatStage(tuxedo.TophatStage):
     template_find_paths = tuxedo.TuxedoBaseStage.template_find_paths[:]
     template_find_paths.insert(0, Path('templates'))
