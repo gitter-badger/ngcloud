217db7fa1aa88ff9f1f7fb05c18011d14c40925f
Weian Wang <stargazer.wang@gmail.com>
2014-12-20 22:21:03 +0800

    .vcf transfer into .html table

--- examples/gatk/gatk.py
+++ examples/gatk/gatk.py
@@ -3,6 +3,7 @@ from ngcloud import _create_logger
 from ngcloud.report import Stage, Report
 from ngcloud.pipe import get_shared_template_root, get_shared_static_root
 from ngcloud.pipe import tuxedo
+from ngcloud.util import open
 
 logger = _create_logger(__name__)
 _here = Path(__file__).parent
@@ -29,12 +30,53 @@ class QCStage(tuxedo.QCStage):
 class AlignStage(GATKBaseStage):
     template_entrances = 'align.html'
 
+
+class VCFStage(GATKBaseStage):
+    # transform vcf into html table 
+    def vcf2html(self, vcffile):
+        vcfile_path = self.result_root / 'gatk' 
+        output_html_path = vcfile_path / str(vcffile + '.html')
+
+        with open(vcfile_path / vcffile) as vcf:
+            start_flag = 0 
+            output = open(output_html_path,'w')
+
+            print('<table id="example" class="table" cellspacing="0" width="100%" style="word-break:break-all">', file = output)
+            # table head
+            for line in vcf:    
+                if line.startswith("#CHROM") == True:
+                    start_flag = 1
+                    if start_flag == 1:
+                        print('<thead>' + '\n' + '\t' + '<tr>', file = output)
+                        col = line.strip().split("\t")
+                        for i in col[0:7]:
+                            print('\t\t' + '<td width = "14.28%">'+i+'</td>', file = output)
+                        break
+            print('\t' + '</tr>' + '\n'+ '</thead>' + '\n', file = output)
+
+            # table body
+            print('<tbody>' + '\n', file = output)
+            for line in vcf:
+                print('\t' + '<tr>', file = output)
+                col = line.strip().split("\t")
+                for i in col[0:7]:
+                    print('\t\t' + '<td>' + i + '</td>', file = output)        
+                print('\t' + '</tr>', file = output)
+            print('</tbody>', file = output)
+
+    def parse(self):
+         self.vcf2html('gatk_result.vcf')
+
+
+
 class GATKStage(GATKBaseStage):
+    template_find_paths = GATKBaseStage.template_find_paths[:]
+    template_find_paths.insert(0,Path('job_gatk', 'gatk'))
     template_entrances = 'gatk.html'
 
 class GATKReport(Report):
     stage_classnames = [
-        IndexStage, QCStage, AlignStage, GATKStage
+        IndexStage, QCStage, AlignStage, VCFStage, GATKStage
     ]
     static_roots = [
         _here / 'report' / 'static',
